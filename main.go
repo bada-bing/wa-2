aa1bcb22f0908c5047c862ffa121126f67e63e2e
Miki <bada.bing.bing@live.com>
2024-07-14 16:02:52 +0200

    ✨ [Orb] Add StartOrbStack command

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"sync"
+	"wa-2/orb"
+)
+
+// this small program will be run inside of tmux session
+// whenever you start a tmuxinator project it will be executed via `on_project_start` hook
+// this is currently: ~/src/dotfiles/scripts/cui-next-work-session.sh
+// that would essentially mean that all project would use the same `start_work_session script`
+
+// 1. tn -> select `work_session` -> start tmuxinator project -> run on_project_start go application
+
+// go application will determine what modules to start (e.g., don't start vpn if it is not a work session)
+// even turn off vpn if it is a study session
+
+// 0. Print "Start work session"
+// 1. Identify the project (work or study), to figure out what tools and how to run them
+// 2. Start necessary tools
+// - 2.1 Start OrbStack ✅
+// - 2.2 Start OpenVPN
+// - 2.3 Arc Browser Management
+// - - - clear the space
+// - - - open only relevant browsers
+// - 2.4 Unit Tests, linting... ❓
+// - 2.5. Open the correct VS Code repository
+// - 2.6. Open the correct Logseq Page
+// 3. Start Clockify Session
+
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	orb.StartOrbStack(&wg)
+	wg.Wait()
+}
---
+++ orb/orb.go
@@ -0,0 +1,26 @@
+package orb
+
+import (
+	"fmt"
+	"log"
+	"os/exec"
+	"sync"
+)
+
+func StartOrbStack(wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	var cmd = exec.Command("bash", "-c", "pgrep -x 'OrbStack' > /dev/null")
+
+	err := cmd.Run()
+	if err != nil {
+		cmd = exec.Command("bash", "-c", "open -j -a OrbStack")
+		err = cmd.Run()
+		if err != nil {
+			log.Fatalf("Failed to start OrbStack: %v", err)
+		}
+		fmt.Println("Start OrbStack")
+	} else {
+		fmt.Println("Orbstack is already running")
+	}
+}
